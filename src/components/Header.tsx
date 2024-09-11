3e73bb60e2789fe2ce6565cd5909123207e12d13
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-12 02:39:59 +0600

    header page related all page set up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <div className="bg-teal-800 py-6">
+        <div className="bg-sky-800 py-6">
             <div className="container mx-auto flex justify-between">
                 <span className="text-3xl text-white font-bold tracking-tight">
                     <Link to='/'>OAIHotelBooking.com</Link>
                 </span>
                 <span className="flex space-x-2">
                     <Link to='/sign-in' 
-                    className='flex items-center text-teal-600 px-3 font-bold bg-white hover:bg-gray-100'>
+                    className='flex items-center text-sky-600 px-3 font-bold bg-white hover:bg-gray-100'>
                         Sign In
                     </Link>
                 </span>
