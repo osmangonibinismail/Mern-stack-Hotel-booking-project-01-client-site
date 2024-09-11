b07f9d38cbea1391dce6aeab22c9f649d1c6f87c
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-12 02:50:24 +0600

    Hero page added

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,14 @@
 
 
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero />
     </div>
   );
 };
