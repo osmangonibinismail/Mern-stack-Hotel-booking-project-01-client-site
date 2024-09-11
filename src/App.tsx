8c2083e4c569f36b2a601b5f01e1631dbf380a9b
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-12 03:29:10 +0600

    footer page and home page added

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,20 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/search" element={<>Search page</>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home page</p>
+            </Layout>
+          } />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search page</p>
+            </Layout>
+          } />
         <Route path="*" element={<Navigate to='/' />} />
       </Routes>
     </Router>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
                     Find your next stay
                 </h1>
                 <p className="text-2xl text-white">
-                    Search low prices on hotels for your dream vacation..
+                    Search low prices on hotels for your dream vacation...
                 </p>
             </div>
         </div>
