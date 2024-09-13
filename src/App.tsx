d4e828387fa0d22bbc3b9df44396deeba2bf7bef
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-13 09:35:52 +0600

    Register page related all page and section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -19,6 +20,12 @@ const App = () => {
               <p>Search page</p>
             </Layout>
           } />
+
+        <Route path="/register" element={<Layout>
+          <Register />
+        </Layout>} />
+
+
         <Route path="*" element={<Navigate to='/' />} />
       </Routes>
     </Router>
