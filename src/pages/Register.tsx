3a1d79bf871bc199e3ced0d64335e20b0631808d
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-14 07:39:56 +0600

    Toast page and related  all section added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from '../api-client';
 import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
     firstName: string,
@@ -13,7 +14,7 @@ export type RegisterFormData = {
 
 
 const Register = () => {
-
+    const navigate = useNavigate();
     const { showToast } = useAppContext();
 
     const { register,
@@ -24,7 +25,8 @@ const Register = () => {
 
     const mutation = useMutation(apiClient.register, {
         onSuccess: () => {
-            showToast({ message: "Registration Success!!", type: "SUCCESS" })
+            showToast({ message: "Registration Success!!", type: "SUCCESS" });
+            navigate("/")
         },
         onError: (error: Error) => {
             showToast({ message: error.message, type: "ERROR" });
