33915b607c760e43f797193475428ab0be2789cd
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-19 23:09:09 +0600

    details  section some section added

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -19,8 +19,8 @@ const Toast = ({ message, type, onClose }: ToastProps) => {
     }, [onClose]);
 
     const styles = type === "SUCCESS"
-        ? "fixed top-4 right-4 z-50 p-2 rounded-md bg-green-600 text-white max-w-md"
-        : "fixed top-4 right-4 z-50 p-2 rounded-md bg-red-600 text-white max-w-md"
+        ? "fixed top-4 right-4 z-50 p-3 rounded-md bg-green-600 text-white max-w-md"
+        : "fixed top-4 right-4 z-50 p-3 rounded-md bg-red-600 text-white max-w-md"
 
     return (
         <div className={styles}>
