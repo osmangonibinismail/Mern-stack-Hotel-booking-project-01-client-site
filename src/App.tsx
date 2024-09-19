41b67d9dac6bf66c1c5a4b7102eeacca03dd51cf
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-19 22:06:06 +0600

    Add hotel page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-d
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 const App = () => {
+  const {isLoggedIn} = useAppContext();
+
   return (
     <Router>
       <Routes>
@@ -32,7 +36,13 @@ const App = () => {
           <SignIn />
         </Layout>} />
 
-
+        {isLoggedIn && (<>
+          <Route path="/add-hotel" element={
+            <Layout>
+              <AddHotel />
+            </Layout>
+          } />
+        </>)}
         <Route path="*" element={<Navigate to='/' />} />
       </Routes>
     </Router>
