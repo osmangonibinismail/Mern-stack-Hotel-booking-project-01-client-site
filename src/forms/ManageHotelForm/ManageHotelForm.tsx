06a6e21b78f42a7fcd1e7ad5beb29db3d81162e9
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-20 11:13:09 +0600

    add hotel page set up updated

--- src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -17,19 +17,54 @@ export type HotelFormData = {
     imageFiles: FileList;
     adultCount: number;
     childCount: number;
+};
+
+type Props = {
+    onSave: (hotelFormData: FormData) => void
+    isLoading: boolean
 }
 
 
-const ManageHotelForm = () => {
+const ManageHotelForm = ({ onSave, isLoading }: Props) => {
     const formMethods = useForm<HotelFormData>();
+    const { handleSubmit } = formMethods;
+    const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+        const formData = new FormData();
+        formData.append("name", formDataJson.name);
+        formData.append("city", formDataJson.city);
+        formData.append("country", formDataJson.country);
+        formData.append("description", formDataJson.description);
+        formData.append("type", formDataJson.type);
+        formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+        formData.append("starRating", formDataJson.starRating.toString());
+        formData.append("adultCount", formDataJson.adultCount.toString());
+        formData.append("childCount", formDataJson.childCount.toString());
+
+        formDataJson.facilities.forEach((facility, index) => {
+            formData.append(`facilities[${index}]`, facility)
+        });
+
+        Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+            formData.append(`imageFiles`, imageFile);
+        });
+
+        onSave(formData)
+    });
     return (
         <FormProvider {...formMethods}>
-            <form className="flex flex-col gap-10">
+            <form className="flex flex-col gap-10" onSubmit={onSubmit}>
                 <HotelDetailsSection />
                 <TypeSection />
                 <FacilitiesSection />
                 <GuestsSection />
                 <ImagesSection />
+                <span className="flex justify-end">
+                    <button
+                        disabled={isLoading}
+                        type="submit" className="bg-sky-700 text-white px-3 py-1 font-semibold hover:bg-sky-600 text-xl min:w-20 disabled:bg-gray-500">
+                        {isLoading ? "Saving..." : "Save"}
+                    </button>
+                </span>
             </form>
         </FormProvider>
     );
