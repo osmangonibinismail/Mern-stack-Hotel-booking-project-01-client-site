dcfd282666b8723f49461ff92d903b278e52a97e
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-24 23:24:26 +0600

    semi-colon added

--- src/pages/AddHotel.tsx
+++ src/pages/AddHotel.tsx
@@ -8,7 +8,7 @@ const AddHotel = () => {
 
     const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
         onSuccess: () => {
-            showToast({ message: "Hotel Saved!", type: "SUCCESS" })
+            showToast({ message: "Hotel Saved!", type: "SUCCESS" });
         },
         onError: () => {
             showToast({ message: "Error Saving Hotel", type: "ERROR" });
