25dcf8bf367e10e4a2c029b7effc092ac92ddf4f
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-26 20:45:13 +0600

    star rating filter page added

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,10 +4,12 @@ import * as apiClient from "../api-client";
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard";
 import Pagination from "../components/Pagination";
+import StarRatingFilter from "../components/StarRatingFilter";
 
 const Search = () => {
     const search = useSearchContext();
-    const [page, setPage] = useState<number>(1)
+    const [page, setPage] = useState<number>(1);
+    const [selectedStars, setSelectedStars] = useState<string[]>([]);
 
     const searchParams = {
         destination: search.destination,
@@ -16,36 +18,52 @@ const Search = () => {
         adultCount: search.adultCount.toString(),
         childCount: search.childCount.toString(),
         page: page.toString(),
+        stars: selectedStars,
     };
 
     const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
         apiClient.searchHotels(searchParams)
     );
 
+
+    const handleStarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const starRating = event.target.value;
+
+        setSelectedStars((prevStars) =>
+            event.target.checked
+                ? [...prevStars, starRating]
+                : prevStars.filter((star) => star !== starRating)
+        );
+    };
+
     return (
         <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
             <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
                 <div className="space-y-5">
                     <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">Filter By:</h3>
-                    {/** TODO filters */}
+
+                    <StarRatingFilter 
+                    selectedStars={selectedStars}
+                    onChange={handleStarChange}/>
+
                 </div>
             </div>
             <div className="flex flex-col gap-5">
                 <div className="flex justify-between items-center">
                     <span className="text-xl font-bold">
-                        {hotelData?.pagination.total} Hotels found 
+                        {hotelData?.pagination.total} Hotels found
                         {search.destination ? `in ${search.destination}` : ""}
                     </span>
                     {/**todo sort options */}
                 </div>
-                {hotelData?.data.map((hotel)=>(
-                    <SearchResultsCard hotel={hotel}/>
+                {hotelData?.data.map((hotel) => (
+                    <SearchResultsCard hotel={hotel} />
                 ))}
                 <div>
-                    <Pagination 
-                    page={hotelData?.pagination.page || 1}
-                    pages={hotelData?.pagination.pages || 1}
-                    onPageChange={(page) => setPage(page)}
+                    <Pagination
+                        page={hotelData?.pagination.page || 1}
+                        pages={hotelData?.pagination.pages || 1}
+                        onPageChange={(page) => setPage(page)}
                     />
                 </div>
             </div>
