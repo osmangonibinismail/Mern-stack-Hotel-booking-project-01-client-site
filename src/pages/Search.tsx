e7034c62eb7b80d0dd9f2379f1ce153113a573f5
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-26 13:12:55 +0600

    search page updated

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,47 @@
-import { useSearchContext } from "../contexts/SearchContexts"
+import { useQuery } from "react-query";
+import { useSearchContext } from "../contexts/SearchContexts";
+import * as apiClient from "../api-client";
+import { useState } from "react";
 
 const Search = () => {
     const search = useSearchContext();
-    console.log(search);
-    
-    return <>Search Page</>;
+    const [page, setPage] = useState<number>(1)
+
+    const searchParams = {
+        destination: search.destination,
+        checkIn: search.checkIn.toISOString(),
+        checkOut: search.checkOut.toISOString,
+        adultCount: search.adultCount.toString(),
+        childCount: search.childCount.toString(),
+        page: page.toString(),
+    };
+
+    const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+        apiClient.searchHotels(searchParams)
+    );
+
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+            <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+                <div className="space-y-5">
+                    <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">Filter By:</h3>
+                    {/** TODO filters */}
+                </div>
+            </div>
+            <div className="flex flex-col gap-5">
+                <div className="flex justify-between items-center">
+                    <span className="text-xl font-bold">
+                        {hotelData?.pagination.total} Hotels found 
+                        {search.destination ? `in ${search.destination}` : ""}
+                    </span>
+                    {/**todo sort options */}
+                </div>
+                {hotelData?.data.map((hotel)=>(
+                    <SearchResultsCard hotel={hotel}/>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Search;
