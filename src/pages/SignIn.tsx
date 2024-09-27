e4ef1825bf4d8725d120d3a8d46ad7fcf184ff1b
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-27 22:52:06 +0600

    sign in and registration page navigate section added

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form"
 import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from '../api-client';
 import { useAppContext } from "../contexts/AppContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export type SignInFormData = {
     email: string;
@@ -13,13 +13,16 @@ const SignIn = () => {
     const queryClient = useQueryClient();
     const { showToast } = useAppContext();
     const navigate = useNavigate();
+
+    const location = useLocation();
+
     const { register, handleSubmit, formState: { errors } } = useForm<SignInFormData>();
 
     const mutation = useMutation(apiClient.signIn, {
         onSuccess: async () => {
             showToast({message: "Sign in Successful!!", type: "SUCCESS"});
             await queryClient.invalidateQueries("validateToken");
-            navigate("/")
+            navigate(location.state?.from?.pathname || "/")
         }, onError: (error: Error) => {
             
             showToast({message: error.message, type: "ERROR"})
